7e444eab4bbf37e5b7b50126d2645254262e3cb1
Your Name <guilleoliva@gmail.com>
2016-09-08 19:00:10 -0300

    Primer ejercicio Python

---
+++ words.py
@@ -0,0 +1,18 @@
+import sys
+from re import findall
+Path=sys.argv[1]
+#print Path
+archivo = open(Path, 'r' )
+linea1 = archivo.readline()
+print linea1
+datos = linea1.split(' ')
+#print datos	
+#print len(datos)
+consonantes = "bcdfghjklmnpqrstvxz"
+nc = 0
+for n in range(len(datos)):
+    nc = len(findall('[%s]' % consonantes, datos[n]))   
+    if nc > 2:
+  		print(datos[n],nc)	
+    nc = 0
+
